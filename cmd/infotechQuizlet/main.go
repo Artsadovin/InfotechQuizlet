c5c5fd5d7b96e0f3814b10949608fccec97cdb61
artsadovin <artsadovin@yandex.ru>
2024-02-10 16:45:27 +0300

    roma's meeting

--- cmd/infotechQuizlet/main.go
+++ cmd/infotechQuizlet/main.go
@@ -1,24 +1,37 @@
 package main
 
 import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/jmoiron/sqlx"
 	"log"
 	"net/http"
 )
 
 const (
-	port = ":3000"
+	port         = ":3000"
+	dbDriverName = "mysql"
 )
 
 func main() {
-	mux := http.NewServeMux()
+
+	db, err := openDB()
 	mux.HandleFunc("/home", index)
-	// Реализуем отдачу статики
 
-	mux.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
+	dbx := sqlx.NewDb(db, dbDriverName)
 
+	mux := mux.NewRouter()
+
+	mux.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
 	log.Println("Start server " + port)
-	err := http.ListenAndServe(port, mux)
+	err = http.ListenAndServe(port, mux)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func OpenDB() (*sql.DB, error) {
+	return sql.Open(dbDriverName, "root:Sadovin512(localhost:3306)/Quizlet?charset=utf8mb4&collation=utf8mb4_unicode_ci&parseTime=true")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module InfotechQuizlet
 
 go 1.20
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/jmoiron/sqlx v1.3.5 // indirect
+)
